4c7353b6074a2a0121cc86c37dc67f7ec44d9914
shona <f.mb@outlook.jp>
2020-07-02 19:42:53 +0900

    studentのviewとEventsControllerの一部を更新
    studentファイルの中にbladeを追加しました。
    EventsControlleのindexのイベント一覧表示できるよう追加しました。

---
+++ warm/resources/views/students/detail.blade.php
@@ -0,0 +1,36 @@
+
+@extends('layouts.students.app')
+@section('content')
+
+<div class="inner">
+
+    
+        <div class="e_time">
+          <div>
+            <span class="event-date">{{$event->date}}</span>
+          </div>
+          <div>
+            <span class="event-time">{{$event->start_time}}</span>
+            <span> - </span>
+            <span class="event-time">{{$event->end_time}}</span>
+          </div>
+        </div>
+        <div class="e_detail">
+          <div class="e_cont">
+          <p class="univ-name">{{大学名}}, <span>{{大学の国}}</span></p>
+          <p class="event-title">{{$event->title}}</p>
+          <p class="event-description">{{$event->dtls}}</p>
+          </div>
+          <div class="e_imgLarger">
+            <img src="{{$event->img}}" class="event_img" alt="">
+          </div>
+        </div>
+          <div class="e_infoLarger">Level: <span>{{レベル}}</span></div> 
+          <div class="e_infoLarger">Subject areas: <span>{{サブジェクト}}</span></div>
+          <div class="e_infoLarger">Suitable for students from <span>{{地域}}</span> region.</div>
+
+    <a href="books" class="e_book">Book</a>
+
+</div>
+
+@endsection('content')
