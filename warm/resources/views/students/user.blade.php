c01f983b080f465df0edc8819be74214a6118815
shona <f.mb@outlook.jp>
2020-07-05 23:24:37 +0900

    学生ユーザーの予約一覧表示処理

--- warm/resources/views/students/user.blade.php
+++ warm/resources/views/students/user.blade.php
@@ -13,16 +13,16 @@
 
     <div class="heading">Your upcoming events</div>
 
-    @foreach($events as $event)
+    @foreach($books as $book)
         <div class="rsv_box mb60">
         <div class="rsv_cont">
         <div>
-        <span class="e-date">{{$event->date}}</span>
-    <span class="event-time">{{$event->start_time}} - </span>
-        <span class="event-time">{{$event->end_time}}</span>
+        <span class="e-date">{{$book->date}}</span>
+    <span class="event-time">{{$book->start_time}} - </span>
+        <span class="event-time">{{$book->end_time}}</span>
     </div>
-        <p class="e_inst">{{大学名}}</p>
-        <p class="e_title_40">{{$event->title}}</p>
+        <p class="e_inst">{{$book->inst_name(大学名)}}</p>
+        <p class="e_title_40">{{$book->title}}</p>
         <a href="{{url('books/'.$event->id)}}"><i class="fas fa-chevron-circle-right"></i>Details</a>
     <a href="{{url('querys/create/'.$event->id)}}"><i class="fas fa-chevron-circle-right"></i>Ask questions</a>
         <a href="{{url('books/'.$event->id)}}"><i class="fas fa-chevron-circle-right"></i>Cancel</a>
