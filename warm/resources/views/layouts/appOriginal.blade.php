9ee5ea137a92d6bd19ca2bb6c5170db47822f183
Ayako Towatari <kyotayako@gmail.com>
2020-07-06 19:31:41 +0900

    更新あり。

--- warm/resources/views/layouts/appOriginal.blade.php
+++ warm/resources/views/layouts/appOriginal.blade.php
@@ -17,20 +17,18 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/main.css">
-    
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <div class="i_nav">
-            <!-- <div class="container"> -->
-                <div>
-                <h1 class="logo">A Warm Welcome</h1>
-                </div>
-                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button> -->
+                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -39,19 +37,19 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <div class="nav-list">
+                    <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            
-                                <a class="nav-item" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
                             @if (Route::has('register'))
-                               
-                                    <a class="nav-item" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
                             @endif
                         @else
-                            
+                            <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
@@ -67,28 +65,16 @@
                                         @csrf
                                     </form>
                                 </div>
-                           
+                            </li>
                         @endguest
-                    </div>
+                    </ul>
                 </div>
-            <!-- </div> -->
-        </div>
+            </div>
+        </nav>
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
-
-    <footer class="i_footer">
-    <small>&copy; A Warm Welcome 2020</small>
-    </footer>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <!-- jqueryとchart.jsの読み込み  -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
-
-    <script src="{{ asset('./js/script.js') }}"></script>
-    <script src="{{ asset('./js/chart.js') }}"></script>
 </body>
 </html>
