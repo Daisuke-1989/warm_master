5b49fd66f5aa63b868453a83c12afff23c400695
shona <f.mb@outlook.jp>
2020-07-06 22:20:13 +0900

    topページ

--- warm/resources/views/students/index.blade.php
+++ warm/resources/views/students/index.blade.php
@@ -1,8 +1,5 @@
-
-
 @extends('layouts.students.app')
 @section('content')
-
 <!-- EYE CATCH -->
 <div class="eye-catch">
 <div class="intro-box">
@@ -10,42 +7,32 @@
     <p class="second-text">Your journey begins here.</p> 
 </div>
 </div>
-
 <div class="inner">
-
     <h1 class="greet">Welcome！</h1>
-
-    <p class="greet">Hello, {{--Auth::lastname()--}} !</p> 
-
-
+    <p class="greet">Hello, {{$user->firstname}} !</p> 
 </div>
-
 <div class="container">
     <h1 class="heading">Search</h1>
-
-
     <div id="search" class="row">
-        <form action="{{url('events')}}" method="post" id="filterResult" name="filterResult" class="col s12">
+        <form action="{{--url('events')--}}" method="post" id="filterResult" name="filterResult" class="col s12">
         {{ csrf_field() }}
-
             <div class="row">
                 <div class="input-field col s12">
                 <select name="dest" id="selectDestination">
                     <option value="" disabled selected>Choose destination</option>
-                        @foreach ($nations as $nation) 
-                        <option value="{{$nation->country}}">{{$nation->country}}</option>
+                       @foreach ($nations as $nation) 
+                        <option value="{{$nation->id}}">{{$nation->country}}</option>
                        @endforeach
                 </select>
                 <label for="dest">Destination</label>
                 </div>
             </div>
-
             <div class="row">
                 <div class="input-field col s12">
                     <select name="rgn" id="selectArea">
                         <option value="" disabled selected>Choose your region</option>
                             @foreach ($nations as $nation)
-                            <option value="{{$nation->region}}">{{$nation->region}}</option> 
+                            <option value="{{ $nation->rgn_id }}">{{ $nation->region }}</option> 
                             @endforeach
                     </select>
                     <label for="rgn">Where you are</label>
@@ -55,8 +42,8 @@
                 <div class="input-field col s12">
                     <select name="lvl" id="selectLevel">
                     <option value="" disabled selected class="choose">Choose level of study</option>
-                            @foreach ($events->level as $level)
-                            <option value="{{$level}}">{{$level}}</option> 
+                            @foreach ($levels as $level)
+                            <option value="{{ $level->id }}">{{ $level->level }}</option> 
                             @endforeach
                     </select>
                     <label for="lvl">Level of study</label>
@@ -65,28 +52,26 @@
                 <button ><span class="btn-filter">Filter</span></button>
         </form>
     </div>
-
             @foreach ($events as $event)
             <div class="e_list">
             <div class="cont_l">
-            <p class="e_date">{{$event->date}}</p>
-            <p class="e_inst">{{$event->inst_name}}, {{$event->country}}</p>
-            <p class="e_title">{{$event->title}}</p>
+            <p class="e_date">{{ $event->date }}</p>
+            <p class="e_inst">{{ $event->inst_name }}, {{ $event->country}}</p>
+            <p class="e_title">{{ $event->title }}</p>
             <div class="flex">
-            <span class="e_info">Level: {{$event->level}}</span>
-            <span class="e_info">Suitable for students in {{$event->region}} region.</span>
+            <span class="e_info">Level: {{ $event->level }}</span>
+            <span class="e_info">Suitable for students in {{ $event->region }} region.</span>
             </div>
             </div>
             <div class="cont_r">
             <div>
-            <img src="{{$event->img}}" class="e_img_thumbnail" alt="">
+            <img src="upload/{{ $event->img}}" class="e_img_thumbnail" alt="">
             </div>
             <div>
-            <a href="{{url('books/'.$event->id)}}"><i class="fas fa-angle-right"></i></a>
+            <a href="/events/{{ $event->id }}"><i class="fas fa-angle-right"></i></a>
             </div>
             </div>
             </div>
             @endforeach
         </div>
-
 @endsection('content')
