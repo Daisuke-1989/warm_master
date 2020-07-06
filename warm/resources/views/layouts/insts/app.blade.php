92abc0d444bac7e6c55ec08f3800dff5b041efe7
Ayako Towatari <kyotayako@gmail.com>
2020-07-06 23:02:53 +0900

    変更を更新。

--- warm/resources/views/layouts/insts/app.blade.php
+++ warm/resources/views/layouts/insts/app.blade.php
@@ -1,46 +1,98 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A Warm Welcome</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;600&family=Open+Sans&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/87fa672dc5.js" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/main.css">
-
+    
 </head>
 <body>
-    <div class="i_nav">
-        <div>
-            <h1 class="logo">A Warm Welcome</h1>
+    <div id="app">
+        <div class="i_nav">
+            <!-- <div class="container"> -->
+                <div>
+                <h1 class="logo">A Warm Welcome</h1>
+                </div>
+                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button> -->
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <div class="nav-list">
+                        <!-- Authentication Links -->
+                        @guest
+                            
+                                <a class="nav-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            
+                            @if (Route::has('register'))
+                               
+                                    <a class="nav-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                
+                            @endif
+                        @else
+                            
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item nav-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                           
+                        @endguest
+                    </div>
+                </div>
+            <!-- </div> -->
         </div>
-        <div class="nav-list">
-            <?php if(!isset($_SESSION["chk_ssid"]) || $_SESSION["chk_ssid"]!=session_id()){ ?>
-            <a href="i_index.php" class="nav-item">Sign up</a>
-            <a href="i_login.php" class="nav-item">Log in</a>
-            <?php }else{ ?>
-            <a href="i_dboard.php" class="nav-item">Dashboard</a>
-            <a href="i_user_logout.php" class="nav-item">Log out</a>
-            <a href="i_user_account.php?id=<?=$iuser_id?>" class="nav-item"><i class="fas fa-user-circle"></i></a>
-            <?php } ?>
-        </div>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
     </div>
-    @yield('content')
 
     <footer class="i_footer">
     <small>&copy; A Warm Welcome 2020</small>
     </footer>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <!-- jqueryとchart.jsの読み込み  -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 
     <script src="{{ asset('./js/script.js') }}"></script>
     <script src="{{ asset('./js/chart.js') }}"></script>
-
 </body>
 </html>
--- warm/resources/views/students/auth/register.blade.php
+++ warm/resources/views/students/auth/register.blade.php
@@ -1,20 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.students.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+<div class="inner">
+
+    <h1 class="greet">Welcome</h1>
+
+</div>
+
+<div class="container">
+
+<p class="heading">Register</p>
+
+    <div class="row">
+                    <form method="POST" action="{{ route('register') }}" class="col s12">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="row">
+                        <div class="input-field col s6">
                             <label for="firstname" class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
 
-                            <div class="col-md-6">
+                            
                                 <input id="firstname" type="text" class="form-control @error('firstname') is-invalid @enderror" name="firstname" value="{{ old('firstname') }}" required autocomplete="firstname" autofocus>
 
                                 @error('firstname')
@@ -22,13 +28,11 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
                         </div>
-
-                        <div class="form-group row">
+                        <div class="input-field col s6">
                             <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
 
-                            <div class="col-md-6">
+                            
                                 <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
 
                                 @error('lastname')
@@ -39,24 +43,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
+                        <div class="row">
+                        <div class="input-field col s12">
+                            <label for="nations_id" class="col-md-4 col-form-label text-md-right">{{ __('Your country') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
-
-                                @error('lastname')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="nations_id" class="col-md-4 col-form-label text-md-right">{{ __('nations_id') }}</label>
-
-                            <div class="col-md-6">
+                            
                                 <input id="nations_id" type="text" class="form-control @error('nations_id') is-invalid @enderror" name="nations_id" value="{{ old('nations_id') }}" required autocomplete="nations_id" autofocus>
 
                                 @error('nations_id')
@@ -67,10 +58,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="year" class="col-md-4 col-form-label text-md-right">{{ __('year') }}</label>
+                        <div class="row">
+                        <div class="input-field col s12">
+                            <label for="year" class="col-md-4 col-form-label text-md-right">{{ __('Which year are you planning to start your study?') }}</label>
 
-                            <div class="col-md-6">
                                 <input id="year" type="text" class="form-control @error('year') is-invalid @enderror" name="year" value="{{ old('year') }}" required autocomplete="year" autofocus>
 
                                 @error('year')
@@ -81,10 +72,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="row">
+                        <div class="input-field col s12">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
@@ -95,10 +87,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="row">
+
+                        <div class="input-field col s12">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -109,25 +102,22 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="row">
+                        <div class="input-field col s12">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
+                            
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn-filter btn-submit">
                                     {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
